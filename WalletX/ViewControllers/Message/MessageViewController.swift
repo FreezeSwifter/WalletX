c3825c0600dd8a74eee1156ce7ab9c51e862bc1e
Why$time <lforme@163.com>
2023-08-10 19:36:07 +0800

    create components

---
+++ WalletX/ViewControllers/Message/MessageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessageViewController.swift
+//  WalletX
+//
+//  Created by DZSB-001968 on 10.8.23.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
