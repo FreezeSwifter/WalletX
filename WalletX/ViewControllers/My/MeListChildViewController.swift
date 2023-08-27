83de72b234c0396bf30eb1929db455dfb57dfd44
Why$time <lforme@163.com>
2023-08-27 23:06:19 +0800

    修改结构

--- WalletX/ViewControllers/My/MeListChildViewController.swift
+++ WalletX/ViewControllers/My/MeListChildViewController.swift
@@ -11,26 +11,41 @@ import RxCocoa
 import RxSwift
 import NSObject_Rx
 
-class MeListChildViewController: UIViewController, JXSegmentedListContainerViewListDelegate {
+protocol MeListChildViewDelegate: AnyObject {
+    
+    func listDidScroll(contentOffsetY: CGFloat)
+}
 
+class MeListChildViewController: UIViewController, JXSegmentedListContainerViewListDelegate {
+    
+    weak var delegate: MeListChildViewDelegate?
+    
     private lazy var tableView: UITableView = {
         let tv = UITableView.init(frame: .zero, style: .plain)
         tv.rowHeight = 216
         tv.delegate = self
         tv.dataSource = self
         tv.backgroundColor = .white
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
         return tv
     }()
-        
+    
     private let listEmptyView: MeListEmptyView = MeListEmptyView(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupView()
+        bind()
     }
     
-    func setupView() {
+    private func bind() {
+        tableView.rx.contentOffset.subscribe(onNext: {[weak self] point in
+            self?.delegate?.listDidScroll(contentOffsetY: point.y)
+        }).disposed(by: rx.disposeBag)
+    }
+    
+    private func setupView() {
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.top.trailing.leading.bottom.equalToSuperview()
@@ -42,7 +57,7 @@ class MeListChildViewController: UIViewController, JXSegmentedListContainerViewL
     func listView() -> UIView {
         return view
     }
-
+    
 }
 
 extension MeListChildViewController: UITableViewDelegate {
@@ -55,16 +70,19 @@ extension MeListChildViewController: UITableViewDelegate {
 extension MeListChildViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if true {
-            tableView.backgroundView = listEmptyView
-        } else {
-            tableView.backgroundView = nil
-        }
-        return 0
+        //        if true {
+        //            tableView.backgroundView = listEmptyView
+        //        } else {
+        //            tableView.backgroundView = nil
+        //        }
+        return 20
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
-        return UITableViewCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = indexPath.row.description
+        cell.backgroundColor = UIColor.qmui_random()
+        return cell
     }
 }
 
